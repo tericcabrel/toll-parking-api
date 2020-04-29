0736c46f59b37d653aba81647a779a6ba4b6d890
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2020-04-29 22:11:32 +0100

    Create listener which set createdAt and updatedAt to now before data saved in the database

---
+++ src/main/java/com/tericcabrel/parking/listeners/ModelPersistListener.java
@@ -0,0 +1,37 @@
+package com.tericcabrel.parking.listeners;
+
+import com.tericcabrel.parking.models.dbs.BaseModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
+import org.springframework.data.mongodb.core.mapping.event.BeforeSaveEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Since all documents have createdAt and updatedAt properties
+ * We listen for the event before the data being persisted in the database and we
+ * set their value to now
+ */
+@Component
+@Slf4j
+public class ModelPersistListener extends AbstractMongoEventListener<BaseModel> {
+    @Override
+    public void onBeforeSave(BeforeSaveEvent<BaseModel> event) {
+        super.onBeforeSave(event);
+
+    }
+
+    @Override
+    public void onBeforeConvert(BeforeConvertEvent<BaseModel> event) {
+        super.onBeforeConvert(event);
+
+        Date dateNow = new Date();
+
+        event.getSource().setCreatedAt(dateNow);
+        event.getSource().setUpdatedAt(dateNow);
+
+        log.info("BeforeConvertEvent - Saving class : " + event.getSource().getClass().getName());
+    }
+}
